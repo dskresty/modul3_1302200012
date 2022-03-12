b60170b97b9a50aeda1174859ed4e9019e4a8223
dskresty <100820249+dskresty@users.noreply.github.com>
2022-03-12 15:53:02 +0700

    modul 3
    Menambahkan class KodeBuah

---
+++ modul3_1302200012(2)/modul3_1302200012(2)/KodeBuah.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302200012_2_
+{
+    internal class KodeBuah
+    {
+        public enum buah
+        {
+            Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry,
+            Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+        };
+
+        public String getKodeBuah(buah Buah)
+        {
+            String[] arrayKode = { "A00", "B00", "C00", "D00", "E00",
+                "F00", "H00",
+                "I00", "J00", "K00", "L00", "M00", "N00", "O00"};
+
+
+            return arrayKode[(int)Buah];
+        }
+    }
+}
---
+++ modul3_1302200012(2)/modul3_1302200012(2)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302200012_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KodeBuah fruit = new KodeBuah();
+
+            String buahBaru = fruit.getKodeBuah(KodeBuah.buah.Apel);
+            Console.WriteLine("Apel -> " + buahBaru);
+            //Console.WriteLine("Aprikot -> " + buahBaru);
+
+            Console.WriteLine("\n");
+        }
+    }
+}
